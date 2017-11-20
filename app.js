b352a122ddb298b4b63330dd7609f411c85d1e79
Riza Fahmi <rizafahmi@gmail.com>
2017-11-20 09:15:37 +0700

    Github compute total stars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = require('express')()
 const responseTime = require('response-time')
 
+const github = require('./lib/github')
+
 app.set('port', process.env.PORT || 3000)
 app.use(responseTime())
 
@@ -8,7 +10,29 @@ app.get('/', (req, res) => {
   res.send('Welcome to caching experience!')
 })
 
+app.get('/api/:username', (req, res) => {
+  const { username } = req.params
+
+  github
+    .getUserRepositories(username)
+    .then(github.computeTotalStars)
+    .then(totalStars => {
+      res.send({
+        totalStars,
+        source: 'GitHub'
+      })
+    })
+    .catch(response => {
+      if (response.response.status === 404) {
+        res.send(
+          'The GitHub username could not be found. Try "rizafahmi" for example'
+        )
+      } else {
+        res.send(response)
+      }
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Magic happen at http://localhost:3000/`)
 })
-
