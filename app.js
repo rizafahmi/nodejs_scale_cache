7189d3d010bf8acca97fbc1efeae0e6844c13a8f
Riza Fahmi <rizafahmi@gmail.com>
2017-11-20 09:47:19 +0700

    Caching with redis

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const app = require('express')()
 const responseTime = require('response-time')
+const redis = require('redis')
 
 const github = require('./lib/github')
+
+const cache = redis.createClient()
+
+cache.on('error', err => console.log(`Redis error: ${err}`))
 
 app.set('port', process.env.PORT || 3000)
 app.use(responseTime())
@@ -10,25 +15,37 @@ app.get('/', (req, res) => {
   res.send('Welcome to caching experience!')
 })
 
-app.get('/api/:username', async (req, res) => {
+app.get('/api/:username', (req, res) => {
   const { username } = req.params
 
-  try {
-    const repos = await github.getUserRepositories(username)
-    const totalStars = await github.computeTotalStars(repos)
-    res.send({
-      totalStars,
-      source: 'GitHub'
-    })
-  } catch (err) {
-    if (err.response.status === 404) {
-      res.send(
-        'The GitHub username could not be found. Try "rizafahmi" for example'
-      )
+  cache.get(username, async (error, totalStars) => {
+    if (error) console.log(error)
+    if (totalStars) {
+      res.send({
+        status: 'OK',
+        totalStars,
+        source: 'Cache'
+      })
     } else {
-      res.send(err)
+      try {
+        const repos = await github.getUserRepositories(username)
+        const totalStars = await github.computeTotalStars(repos)
+        cache.setex(username, 60, totalStars)
+        res.send({
+          totalStars,
+          source: 'GitHub'
+        })
+      } catch (err) {
+        if (err.response.status === 404) {
+          res.send(
+            'The GitHub username could not be found. Try "rizafahmi" for example'
+          )
+        } else {
+          res.send(err)
+        }
+      }
     }
-  }
+  })
 })
 
 app.listen(app.get('port'), () => {
